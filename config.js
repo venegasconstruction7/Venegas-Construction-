a0a3766925c019e6fa701b35746fa3a35c3c6a4b
venegasconstruction7 <venegasconstruction818@gmail.com>
2026-08-21 23:39:26 -0600

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Replace these two values with your Supabase project values.
-// Supabase Dashboard -> Project Settings -> API
-export const SUPABASE_URL = "PASTE_SUPABASE_PROJECT_URL_HERE";
-export const SUPABASE_PUBLISHABLE_KEY = "PASTE_SUPABASE_PUBLISHABLE_KEY_HERE";
